ae2bcfa38aca2c6dad7c22af35537c1accf5ee22
Idemudia <idemudiamondayosaghae@gmail.com>
2023-02-17 20:16:34 +0200

    amend error on httpClient.get(uri)

--- lib/services/weather+api_services.dart
+++ lib/services/weather+api_services.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+
 import 'package:open_weather_cubit/constants/constants.dart';
 import 'package:open_weather_cubit/exceptions/weather_exception.dart';
 import 'package:open_weather_cubit/models/direct_geocoding.dart';
@@ -13,36 +14,36 @@ class WeatherApiServices {
   WeatherApiServices({
     required this.httpClient,
   });
-}
 
-Future<DirectGeocoding> getDirectGeocoding(String city) async {
-  final Uri uri = Uri(
-    scheme: 'https',
-    host: kApiHost,
-    path: '/geo/1.0/direct',
-    queryParameters: {
-      'q': city,
-      'limit': kLimit,
-      'appid': dotenv.env['APPID'],
-    },
-  );
+  Future<DirectGeocoding> getDirectGeocoding(String city) async {
+    final Uri uri = Uri(
+      scheme: 'https',
+      host: kApiHost,
+      path: '/geo/1.0/direct',
+      queryParameters: {
+        'q': city,
+        'limit': kLimit,
+        'appid': dotenv.env['APPID'],
+      },
+    );
 
-  try {
-    final http.Response response = await http.Client.get(uri);
+    try {
+      final http.Response response = await httpClient.get(uri);
 
-    if (response.statusCode != 200) {
-      throw httpErrorHandler(response);
+      if (response.statusCode != 200) {
+        throw httpErrorHandler(response);
+      }
+      final responseBody = json.decode(response.body);
+
+      if (responseBody.isEmpty) {
+        throw WeatherException('Cannot get the location of $city');
+      }
+
+      final directGeocoding = DirectGeocoding.fromJson(responseBody);
+
+      return directGeocoding;
+    } catch (e) {
+      rethrow;
     }
-    final responseBody = json.decode(response.body);
-
-    if (responseBody.isEmpty) {
-      throw WeatherException('Cannot get the location of $city');
-    }
-
-    final directGeocoding = DirectGeocoding.fromJson(responseBody);
-
-    return directGeocoding;
-  } catch (e) {
-    rethrow;
   }
 }
