eecce44de6761f3b3c638197031338336a69e240
Idemudia <idemudiamondayosaghae@gmail.com>
2023-02-18 11:36:09 +0200

    create actions-button on home_page and a form on search_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:open_weather_cubit/cubits/weather/weather_cubit.dart';
+import 'package:open_weather_cubit/pages/search_page.dart';
 // import 'package:http/http.dart' as http;
 // import 'package:open_weather_cubit/repositories/weather_repository.dart';
 // import 'package:open_weather_cubit/services/weather_api_services.dart';
@@ -13,7 +14,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
+    String? _city;
+
   ////create a call for testing
   // @override
   // void initState() {
@@ -34,6 +36,23 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Weather'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () async {
+              _city = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return SearchPage();
+                }),
+              );
+              print('city: $_city');
+                 if (_city != null) {
+                context.read<WeatherCubit>().fetchWeather(_city!);
+              }
+            }
+          )
+        ]
       ),
       body: Center(
         child: Text('Home'),
